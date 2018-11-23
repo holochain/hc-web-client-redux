d8ce2dd8235912cf1f9149995e8de3f8a8a1fc46
Michael Dougherty <maackle.d@gmail.com>
2018-11-23 00:05:38 -0500

    Update to allow calling arbitrary methods

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ type Hash = string
 export const connect = (url) => new Promise((fulfill, reject) => {
   const ws = new Client(url)
   ws.on('open', () => {
-    const call = (dnaHash, zome, capability, func) => (params) => {
-      return ws.call(`${dnaHash}/${zome}/${capability}/${func}`, params)
+    const call = (...segments) => (params) => {
+      const method = segments.length === 1 ? segments[0] : segments.join('/')
+      return ws.call(method, params)
     }
-    const close = ws.close
+    const close = () => ws.close()
     fulfill({call, close, ws})
   })
 })
