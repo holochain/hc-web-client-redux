6db59e04c1b78e9a68f06e545d1c0ddac541e9f5
Willem Olding <willemolding@gmail.com>
2018-12-14 12:32:24 +1100

    ads dist to gitignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type hcWebClientConnect = {
-    call: (callStr: string) => (params: any) => Promise<string>;
-    close: () => Promise<any>;
-    ws: any;
-};
-export declare const connect: (url: string) => Promise<hcWebClientConnect>;
-export {};
