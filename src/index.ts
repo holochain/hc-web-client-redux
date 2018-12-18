2b0a872843cb81be3d94f5efccd81847ae25851f
Willem Olding <willemolding@gmail.com>
2018-12-18 15:14:13 +1100

    adds Array<string> type for call

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {Client} from 'rpc-websockets'
 type Hash = string
 
 type HcWebClientConnect = {
-  call: (...methodSegments: string) => (params: any) => Promise<string>,
+  call: (...methodSegments: Array<string>) => (params: any) => Promise<string>,
   close: () => Promise<any>,
   ws: any
 }
