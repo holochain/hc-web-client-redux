83f97a7c8e09e293eb2ef8b0d95dcc875d201b87
Connor Turland <connor.turland@holo.host>
2019-02-01 20:07:05 +0100

    add comments, and renames

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import {Client} from 'rpc-websockets'
 type Hash = string
 
+// export a function which consumes a URL of a running Holochain Websocket based JSON-RPC service
+// and returns a Promise which fulfills to some utility functions for interacting with Holochain
 export const connect = (url) => new Promise((fulfill, reject) => {
   const ws = new Client(url)
   ws.on('open', () => {
-    const call = (...segments) => (params) => {
-      const method = segments.length === 1 ? segments[0] : segments.join('/')
+    // create a function which returns a function
+    // the outer function should be called with one or more arguments, meant to represent the RPC "method"
+    // this value can only be a string
+    // so, if there is only one, that argument will remain as-is
+    // if there is more than one, the arguments will be joined into a single string, separated by forward slashes '/'
+
+    // the inner function should be called with a JSON object, which will be the RPC "params"
+    
+    // in practice, using this looks like `call("instance", "zomename", "function_name")({ key: "value" })`
+    
+    // the whole thing returns a Promise, which will resolve to the result, or an error
+    const call = (...method_segments) => (params) => {
+      
+      const method = method_segments.length === 1 ? method_segments[0] : method_segments.join('/')
       return ws.call(method, params)
     }
+    // define a function which will close the websocket connection
     const close = () => ws.close()
+    
+    // fulfill with the Promise with the `call`, and `close` functions, and the raw WebSocket too
     fulfill({call, close, ws})
   })
 })
 
+// in case for use in the browser, expose the `connect` function under the holochain_client global variable
 const win = (window as any)
-win.holoclient = win.holoclient || {connect}
+win.holochain_client = win.holochain_client || {connect}
