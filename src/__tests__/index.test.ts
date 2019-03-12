f227be4fdb4bddb967b52a9af9b97bde93fafec0
Willem Olding <willemolding@gmail.com>
2019-03-12 12:26:20 +1100

    adds new call style with tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,19 +1,24 @@
 import { connect } from '../index'
 import { Client } from 'rpc-websockets'
 
+const callMock = jest.fn()
+
 jest.mock('rpc-websockets', () => {
   return {
     Client: jest.fn(() => {
       return {
         on: jest.fn((on, callback) => {
           return callback()
-        })
+        }),
+        call: jest.fn((method, params) => {
+          callMock(method, params)
+        }),
       }
     })
   }
 })
 
-describe('hc-web-client', () => {
+describe('hc-web-client connect', () => {
 
   it('Can connect by passing URL', async () => {
     const testUrl = 'ws://localhost:3000'
@@ -33,6 +38,34 @@ describe('hc-web-client', () => {
     // @ts-ignore
     fetch.mockResponseOnce(JSON.stringify({ }))
     expect(connect()).rejects.toEqual(`Could not auto-detect DNA interface from conductor. Ensure the web UI is hosted by a Holochain Conductor or manually specify url as parameter to connect`)
+  })
+
+})
+
+describe('hc-web-client call', () => {
+
+  it('produces the expected call object when called with three params', async () => {
+    const testUrl = 'ws://localhost:3000'
+    const { call } = await connect(testUrl)
+    await call('instance', 'zome', 'func')({param1: 'x'})
+    expect(callMock).toBeCalledWith('call', {
+      'instance_id': 'instance',
+      'zome': 'zome',
+      'function': 'func',
+      'params': {param1: 'x'}
+    })
+  })
+
+  it('produces the expected call object when called with one slash delimited string', async () => {
+    const testUrl = 'ws://localhost:3000'
+    const { call } = await connect(testUrl)
+    await call('instance/zome/func')({param2: 'y'})
+    expect(callMock).toBeCalledWith('call', {
+      'instance_id': 'instance',
+      'zome': 'zome',
+      'function': 'func',
+      'params': {param2: 'y'}
+    })
   })
 
 })
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,28 @@ Ensure the web UI is hosted by a Holochain Conductor or manually specify url as 
   const ws = new Client(url)
   ws.on('open', () => {
     const call = (...methodSegments) => (params) => {
-      const method = methodSegments.length === 1 ? methodSegments[0] : methodSegments.join('/')
-      return ws.call(method, params)
+      let instanceId
+      let zome
+      let func
+
+      if (methodSegments.length === 1) {
+        [instanceId, zome, func] = methodSegments[0].split('/')
+      } else if (methodSegments.length === 3) {
+        [instanceId, zome, func] = methodSegments
+      } else {
+        throw new Error('Invalid params to call. \
+          Must be either 3 string spefifying instance, zome, function \
+          or a single string delimited with "/"')
+      }
+
+      const callObject = {
+        'instance_id': instanceId,
+        zome,
+        'function': func,
+        params
+      }
+
+      return ws.call('call', callObject)
     }
     // define a function which will close the websocket connection
     const close = () => ws.close()
