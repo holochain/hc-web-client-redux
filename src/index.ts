7b3060b5e1d9d6803f7c3f594121926cdad8fee1
Michael Dougherty <maackle.d@gmail.com>
2019-05-19 17:44:00 -0700

    Use callWhenConnected

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,14 @@ export const connect = (paramUrl?: string) => new Promise<{call: Call, callZome:
 Ensure the web UI is hosted by a Holochain Conductor or manually specify url as parameter to connect'))
 
   const ws = new Client(url)
-  ws.on('open', () => {
+
+  ws.on('open', () => 'WS open')
+  ws.on('close', () => 'WS closed')
+
+  ws.once('open', () => {
     const call = (...methodSegments) => (params) => {
       const method = methodSegments.length === 1 ? methodSegments[0] : methodSegments.join('/')
-      return ws.call(method, params)
+      return callWhenConnected(ws, method, params)
     }
     const callZome = (instanceId, zome, func) => (params) => {
       const callObject = {
@@ -25,7 +29,7 @@ Ensure the web UI is hosted by a Holochain Conductor or manually specify url as 
         'function': func,
         params
       }
-      return ws.call('call', callObject)
+      return callWhenConnected(ws, 'call', callObject)
     }
     const onSignal: OnSignal = (callback: (params: any) => void) => {
       // go down to the underlying websocket connection (.socket)
@@ -50,5 +54,26 @@ function getUrlFromContainer (): Promise<string> {
     .then(json => json.dna_interface.driver.port)
     .then(port => `ws://localhost:${port}`)
 }
+
+/**
+ * Ensure that a ws client never attempts to call when the socket is not ready
+ * Instead, return a promise that resolves only when the socket is connected and the call is made
+ */
+async function callWhenConnected (ws, method, payload, timeout=3000) {
+  if (ws.ready) {
+    return Promise.resolve(ws.call(method, payload))
+  } else {
+    return new Promise((resolve, reject) => {
+      const timer = setTimeout(() => {
+        reject(`Timeout while waiting for ws to connect. method: ${method}, payload: ${JSON.stringify(payload)}`)
+      }, timeout)
+      ws.once('open', () => {
+        clearTimeout(timer)
+        ws.call(method, payload).then(resolve).catch(reject)
+      })
+    })
+  }
+}
+
 
 const holochainclient = { connect }
