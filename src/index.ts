095c2b558dec71e6f88be571953f36b22f247216
Nicolas Luck <nicolas@lucksus.eu>
2019-12-12 13:13:06 +0100

    Don't filter signal objects for property signal since we now have conductor signals

--- src/index.ts
+++ src/index.ts
@@ -55,9 +55,7 @@ Ensure the web UI is hosted by a Holochain Conductor or manually specify url as 
       ws.socket.on('message', (message: any) => {
         if (!message) return
         const msg = JSON.parse(message)
-        if (msg.signal) {
-          callback(msg)
-        }
+        callback(msg)
       })
     }
     // define a function which will close the websocket connection
